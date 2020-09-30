e021312e2269313fc85d4a52224a416fd8e7b806
sunjiahui <1455287434@qq,com>
2020-09-30 11:12:53 +0800

    合并

--- src/views/energySystem/energyConfig/energyType/index.vue
+++ src/views/energySystem/energyConfig/energyType/index.vue
@@ -1,56 +1,57 @@
 <template>
-  <div class="index">
+<div class="index">
     <div class="main-wrap" id="mainWrap">
-<!--      <el-button @click="handleFullScreen">全屏</el-button>-->
-    <div class="screen-title">123321</div>
-    <div style="width: 20vw;height: 20vh;">
-      <dv-border-box-1>dv-border-box-1</dv-border-box-1>
+        <!--      <el-button @click="handleFullScreen">全屏</el-button>-->
+        <div class="screen-title">123321</div>
+        <div style="width: 20vw; height: 20vh">
+            <dv-border-box-1>dv-border-box-1</dv-border-box-1>
+        </div>
     </div>
-    </div>
-  </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: 'index',
-  methods: {
-    // 全屏事件
-    handleFullScreen () {
-      const element = document.getElementById('mainWrap')
-      if (this.fullscreen) {
-        if (document.exitFullscreen) {
-          document.exitFullscreen()
-        } else if (document.webkitCancelFullScreen) {
-          document.webkitCancelFullScreen()
-        } else if (document.mozCancelFullScreen) {
-          document.mozCancelFullScreen()
-        } else if (document.msExitFullscreen) {
-          document.msExitFullscreen()
-        }
-      } else {
-        if (element.requestFullscreen) {
-          element.requestFullscreen()
-        } else if (element.webkitRequestFullScreen) {
-          element.webkitRequestFullScreen()
-        } else if (element.mozRequestFullScreen) {
-          element.mozRequestFullScreen()
-        } else if (element.msRequestFullscreen) {
-          // IE11
-          element.msRequestFullscreen()
-        }
-      }
-      this.fullscreen = !this.fullscreen
-    }
-  }
-}
+    name: "index",
+    methods: {
+        // 全屏事件
+        handleFullScreen() {
+            const element = document.getElementById("mainWrap");
+            if (this.fullscreen) {
+                if (document.exitFullscreen) {
+                    document.exitFullscreen();
+                } else if (document.webkitCancelFullScreen) {
+                    document.webkitCancelFullScreen();
+                } else if (document.mozCancelFullScreen) {
+                    document.mozCancelFullScreen();
+                } else if (document.msExitFullscreen) {
+                    document.msExitFullscreen();
+                }
+            } else {
+                if (element.requestFullscreen) {
+                    element.requestFullscreen();
+                } else if (element.webkitRequestFullScreen) {
+                    element.webkitRequestFullScreen();
+                } else if (element.mozRequestFullScreen) {
+                    element.mozRequestFullScreen();
+                } else if (element.msRequestFullscreen) {
+                    // IE11
+                    element.msRequestFullscreen();
+                }
+            }
+            this.fullscreen = !this.fullscreen;
+        },
+    },
+};
 </script>
 
 <style scoped>
 .main-wrap {
-  background-color: #0c1941;
+    background-color: #0c1941;
 }
+
 .screen-title {
-  height: 15%;
-  background-color: #fff;
+    height: 15%;
+    background-color: #fff;
 }
 </style>
