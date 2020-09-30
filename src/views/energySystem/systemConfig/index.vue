2cd090c850e6170a044f10af88262726bea4a116
sunjiahui <1455287434@qq,com>
2020-09-30 10:23:54 +0800

    j解决冲突

--- src/views/energySystem/systemConfig/index.vue
+++ src/views/energySystem/systemConfig/index.vue
@@ -67,13 +67,11 @@
 
 <script>
 export default {
-    <<
-    <<
-    <<
-    < HEAD
     name: 'index',
     data() {
         return {
+            dialogVisible: false,
+            title: '新增',
             selectForm: {
                 user: ''
             },
@@ -93,126 +91,96 @@ export default {
                 date: '2016-05-03',
                 name: '王小虎',
                 address: '上海市普陀区金沙江路 1516 弄'
-            }]
-        } ===
-        ===
-        =
-        name: 'index',
-            data() {
-                return {
-                    dialogVisible: false,
-                    title: '新增',
-                    selectForm: {
-                        user: ''
-                    },
-                    tableData: [{
-                        date: '2016-05-02',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1518 弄'
-                    }, {
-                        date: '2016-05-04',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1517 弄'
-                    }, {
-                        date: '2016-05-01',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1519 弄'
-                    }, {
-                        date: '2016-05-03',
-                        name: '王小虎',
-                        address: '上海市普陀区金沙江路 1516 弄'
-                    }],
-                    ruleForm: {
-                        name: ''
-                    },
-                    rules: {
-                        name: [{
-                                required: true,
-                                message: '请输入活动名称',
-                                trigger: 'blur'
-                            },
-                            {
-                                min: 3,
-                                max: 5,
-                                message: '长度在 3 到 5 个字符',
-                                trigger: 'blur'
-                            }
-                        ]
-                    },
-                    multipleSelection: []
-                }
+            }],
+            ruleForm: {
+                name: ''
             },
-            methods: {
-                // 获取列表
-                getList() {
-                    // do something
-                },
-                // 添加方法
-                addFn(type) {
-                    this.dialogVisible = !this.dialogVisible
-                    if (type === 'add') {
-                        this.title = '新增'
-                    } else {
-                        this.title = '修改'
+            rules: {
+                name: [{
+                        required: true,
+                        message: '请输入活动名称',
+                        trigger: 'blur'
+                    },
+                    {
+                        min: 3,
+                        max: 5,
+                        message: '长度在 3 到 5 个字符',
+                        trigger: 'blur'
                     }
-                },
-                // 添加校验
-                submitForm(formName) {
-                    this.$refs[formName].validate((valid) => {
-                        if (valid) {
-                            alert('submit!')
-                        } else {
-                            console.log('error submit!!')
-                            return false
-                        }
-                    })
-                    this.dialogVisible = false
-                },
-                // 添加，修改函数
-                submitFn() {
+                ]
+            },
+            multipleSelection: []
+        }
+    },
+    methods: {
+        // 获取列表
+        getList() {
+            // do something
+        },
+        // 添加方法
+        addFn(type) {
+            this.dialogVisible = !this.dialogVisible
+            if (type === 'add') {
+                this.title = '新增'
+            } else {
+                this.title = '修改'
+            }
+        },
+        // 添加校验
+        submitForm(formName) {
+            this.$refs[formName].validate((valid) => {
+                if (valid) {
+                    alert('submit!')
+                } else {
+                    console.log('error submit!!')
+                    return false
+                }
+            })
+            this.dialogVisible = false
+        },
+        // 添加，修改函数
+        submitFn() {
 
-                },
-                deleteFn(val, type) {
-                    if (type === 'select') {
-                        if (this.multipleSelection.length === 0) {
-                            this.$message.error('请选择需要删除的项目')
-                            return
-                        }
-                    }
-                    this.$confirm('此操作将永久删除, 是否继续?', '提示', {
-                        confirmButtonText: '确定',
-                        cancelButtonText: '取消',
-                        type: 'warning'
-                    }).then(() => {
-                        this.$message({
-                            type: 'success',
-                            message: '删除成功!'
-                        })
-                        this.getList()
-                    }).catch(() => {
-                        this.$message({
-                            type: 'info',
-                            message: '已取消删除'
-                        })
-                    })
-                },
-                handleSelectionChange(val) {
-                    this.multipleSelection = val
-                },
-                searchFn() {
-                    this.getList()
-                },
-                resetFn() {
-                    this.getList()
-                },
-                resetForm(formName) {
-                    this.$refs[formName].resetFields()
-                    this.dialogVisible = false >>>
-                        >>>
-                        >
-                        3 ba1e10db04036c9d681699c03c38e91f8cea2b8
+        },
+        deleteFn(val, type) {
+            if (type === 'select') {
+                if (this.multipleSelection.length === 0) {
+                    this.$message.error('请选择需要删除的项目')
+                    return
                 }
             }
+            this.$confirm('此操作将永久删除, 是否继续?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).then(() => {
+                this.$message({
+                    type: 'success',
+                    message: '删除成功!'
+                })
+                this.getList()
+            }).catch(() => {
+                this.$message({
+                    type: 'info',
+                    message: '已取消删除'
+                })
+            })
+        },
+        handleSelectionChange(val) {
+            this.multipleSelection = val
+        },
+        searchFn() {
+            this.getList()
+        },
+        resetFn() {
+            this.getList()
+        },
+        resetForm(formName) {
+            this.$refs[formName].resetFields()
+            this.dialogVisible = false
+        }
+    }
+}
 </script>
 
 <style scoped>
